79d96d6174dff8c0d78b89e7cfc94adb736b8b7d
Talon Hughes <talon.hughes@gmail.com>
2016-10-29 11:19:46 -0600

    finished

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
+var connectionString = "postgres://postgres:testmini@localhost/sandbox";
 
 var app = module.exports = express();
+
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen('3000', function(){
-  console.log("Successfully listening on : 3000")	
-})
+var massiveInstance = massive.connectSync({connectionString : connectionString});
 
+app.set('db', massiveInstance);
+
+// var db = app.get('db');
+
+// db.new_plane(function(err, planes){
+//     console.log(err, "plane added");
+// });
+
+// db.get_planes(function(err, planes){
+//     console.log(err, planes);
+// });
+
+var controller = require('./controller.js');
+
+controller.getPlanes();
+
+app.listen('3000', function(){
+  console.log("Successfully listening on : 3000");
+});
